2d5b003f55124afd7b0dc74cdc3027c54075da34
ManiDeep Reddy <deepmani5903@gmail.com>
2023-11-04 11:04:20 +0530

    hh

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,26 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginFrom from './compontes/Login';
+import { useState } from 'react';
 
 function App() {
  const[islogin,setIsLogin] =  useState(false)
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <LoginFrom/>
+  )
+  
 }
 
 export default App;
